81745d5906acb1c680010a6210831f08996e5e71
Jarrett <jarrett.yew@gmail.com>
2020-02-20 20:55:11 +0800

    Adds CSS to the searchbar and Bigelow font

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 import { CardList } from './components/card-list/card-list.component'
 import './App.css'
+import SearchBox from './components/search-box/search-box.component'
 
 class App extends Component {
   constructor() {
     super()
     this.state = {
       monsters: [],
+      searchField: '',
     }
   }
 
@@ -16,10 +18,23 @@ class App extends Component {
       .then(users => this.setState({ monsters: users }))
   }
 
+  handleChange = e => {
+    this.setState({ searchField: e.target.value })
+  }
+
   render() {
+    const { monsters, searchField } = this.state
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    )
     return (
       <div className="App">
-        <CardList monsters={this.state.monsters} />
+        <h1>Monsters Rolodex</h1>
+        <SearchBox
+          placeholder="Search monsters"
+          handleChange={this.handleChange}
+        />
+        <CardList monsters={filteredMonsters} />
       </div>
     )
   }
